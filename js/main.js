6302a006451cff9d46002560051b97162cf83c99
taverasmisael <macrot29@gmail.com>
2018-10-12 14:38:03 -0400

    encansuplate into a IIFE and move Generator out the DOMLoaded scope

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-document.addEventListener('DOMContentLoaded', function onDocumentReady() {
+'use srtrict'
+;(function App() {
   function GenerateRandomPassword(length = 24, type = 'complex') {
     var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ'
     var alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ1234567890'
@@ -24,52 +25,56 @@ document.addEventListener('DOMContentLoaded', function onDocumentReady() {
     return password
   }
 
-  function GetSelectedRadio(radios) {
-    for (var i = 0, length = radios.length; i < length; i++) {
-      if (radios[i].checked) {
-        return radios[i].value
+  document.addEventListener('DOMContentLoaded', function onDocumentReady() {
+    function GetSelectedRadio(radios) {
+      for (var i = 0, length = radios.length; i < length; i++) {
+        if (radios[i].checked) {
+          return radios[i].value
+        }
       }
     }
-  }
-  new ClipboardJS('#copyButton')
+    new ClipboardJS('#copyButton')
 
-  hsimp(
-    {
-      options: {
-        calculationsPerSecond: 1e10, // 10 billion,
-        good: 31557600e6, // 1,000,000 years
-        ok: 31557600e2 // 100 year
+    hsimp(
+      {
+        options: {
+          calculationsPerSecond: 1e10, // 10 billion,
+          good: 31557600e6, // 1,000,000 years
+          ok: 31557600e2 // 100 year
+        },
+        outputTime: function(time, input) {
+          document.querySelector('#time').innerHTML = time || 'instantly'
+        }
       },
-      outputTime: function(time, input) {
-        document.querySelector('#time').innerHTML = time || 'instantly'
-      }
-    },
-    document.getElementById('randomPassword')
-  )
+      document.getElementById('randomPassword')
+    )
 
-  document.getElementById('randomPasswordLength').oninput = function() {
-    document.getElementById('randomPasswordLengthValue').innerText = this.value
-    document.getElementById('randomPassword').value = GenerateRandomPassword(
-      this.value,
-      GetSelectedRadio(document.getElementsByName('type'))
-    )
-    /* Tiggering keyup event, vanillay way */
-    document.getElementById('randomPassword').dispatchEvent(new Event('keyup'))
-  }
-  document.querySelector('#generatePassword').addEventListener('submit', function(event) {
-    event.preventDefault()
-    document.getElementById('randomPassword').value = GenerateRandomPassword(
-      document.getElementById('randomPasswordLength').value
-    )
+    document.getElementById('randomPasswordLength').addEventListener('input', function() {
+      document.getElementById('randomPasswordLengthValue').innerText = this.value
+      document.getElementById('randomPassword').value = GenerateRandomPassword(
+        this.value,
+        GetSelectedRadio(document.getElementsByName('type'))
+      )
+      /* Tiggering keyup event, vanillay way */
+      document.getElementById('randomPassword').dispatchEvent(new Event('keyup'))
+    })
+    document.querySelector('#generatePassword').addEventListener('submit', function(event) {
+      event.preventDefault()
+      document.getElementById('randomPassword').value = GenerateRandomPassword(
+        document.getElementById('randomPasswordLength').value
+      )
+      /* Tiggering keyup event, vanillay way */
+      document.getElementById('randomPassword').dispatchEvent(new Event('keyup'))
+    })
+
+    document.getElementById('randomPasswordLengthValue').innerText = document.getElementById(
+      'randomPasswordLength'
+    ).value
+    document.getElementById('randomPassword').value = document.getElementById(
+      'randomPassword'
+    ).value = GenerateRandomPassword(this.value, GetSelectedRadio(document.getElementsByName('type')))
+
     /* Tiggering keyup event, vanillay way */
     document.getElementById('randomPassword').dispatchEvent(new Event('keyup'))
   })
-
-  document.getElementById('randomPasswordLengthValue').innerText = document.getElementById('randomPasswordLength').value
-  document.getElementById('randomPassword').value = document.getElementById(
-    'randomPassword'
-  ).value = GenerateRandomPassword(this.value, GetSelectedRadio(document.getElementsByName('type')))
-
-  /* Tiggering keyup event, vanillay way */
-  document.getElementById('randomPassword').dispatchEvent(new Event('keyup'))
-})
+})()
