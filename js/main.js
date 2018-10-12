21abfd70f6e2d1e63bfad95de8f3536e766a2461
taverasmisael <macrot29@gmail.com>
2018-10-12 16:53:20 -0400

    simplify reducer in GenerateRandomPassword. Pass elements implicitly

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 ;(function App() {
   // this function wouldn't be a anonymous function for debugging purposes
   document.addEventListener('DOMContentLoaded', onDocumentReady) // thanks to the hoisting power
+
+  const randomIndex = seed => Math.floor(Math.random() * seed)
   const GenerateRandomPassword = (length = 12, type = 'complex') => {
     const Dictionaries = {
       letters: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVXYZ',
@@ -13,16 +15,14 @@
     const charsLength = chars.length
     return Array(size)
       .fill(0)
-      .reduce((prev, _) => prev.concat(chars.charAt(randomIndex(chars.length))), '')
+      .reduce(prev => prev.concat(chars.charAt(randomIndex(charsLength))), '')
   }
-
-  const randomIndex = seed => Math.floor(Math.random() * seed)
 
   const initClipboard = selector => new ClipboardJS(selector)
 
   const getActiveRadio = radios => [...radios].find(radio => radio.checked) || {}
 
-  const initHSIMP = (selectorId, outputEl) =>
+  const initHSIMP = (passwordElement, outputElement) =>
     hsimp(
       {
         options: {
@@ -30,16 +30,16 @@
           good: 31557600e6, // 1,000,000 years
           ok: 31557600e2 // 100 year
         },
-        outputTime: time => (outputEl.innerHTML = time || 'instantly')
+        outputTime: time => (outputElement.innerHTML = time || 'instantly')
       },
-      document.getElementById(selectorId)
+      passwordElement
     )
 
   const triggerEvent = (element, eventType) => element.dispatchEvent(new Event(eventType))
 
   function onDocumentReady() {
     initClipboard('#copyButton')
-    initHSIMP('randomPassword', document.querySelector('#time'))
+    initHSIMP(document.getElementById('randomPassword'), document.getElementById('time'))
 
     const PasswordLengthInput = document.getElementById('randomPasswordLength')
     const PasswordLengthLabel = document.getElementById('randomPasswordLengthLabel')
@@ -68,7 +68,7 @@
     })
 
     PasswordLengthLabel.innerText = PasswordLengthInput.value
-    GeneratedPasswordInput.value = GeneratedPasswordInput.value = GenerateRandomPassword(undefined, getPasswordType())
+    GeneratedPasswordInput.value = GenerateRandomPassword(undefined, getPasswordType())
 
     // Trigger the first 'onChange' like event to update the view
     triggerPasswordChange()
